b323c9da1d7dff7f366d9f0a9ae118484e8a1afb
Jeff <jfx319@users.noreply.github.com>
2016-12-02 23:32:44 -0500

    Update vgg16_adapt.py

--- crop/mass/benignvsmalign/vgg16/vgg16_adapt.py
+++ crop/mass/benignvsmalign/vgg16/vgg16_adapt.py
@@ -5,35 +5,16 @@ import numpy as np
 from datetime import datetime
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, SpatialDropout2D
+#from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, SpatialDropout2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, ReduceLROnPlateau, CSVLogger
-from keras.layers import BatchNormalization
-
+#from keras.layers import BatchNormalization
 
 from keras.applications.vgg16 import VGG16
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
-
-model = VGG16(weights='imagenet', include_top=False)
-
-img_path = 'elephant.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-
-features = model.predict(x)
-
 
 ########################################
 
-# path to the model weights file.
-weights_path = '../keras/examples/vgg16_weights.h5'
-top_model_weights_path = 'bottleneck_fc_model.h5'
-
-  benign,  malignant
 basedir = './'
 train_data_dir = basedir+'/train'
 validation_data_dir = basedir+'/validate'
@@ -63,21 +44,21 @@ def save_bottlebeck_features():
     
     datagen = ImageDataGenerator(rescale=1./65535)
     generator = datagen.flow_from_directory(
-            train_data_dir,
-            target_size=(img_width, img_height),
-            batch_size=33,   #1881 divisible by 33
-            class_mode=None, # predict method doesn't accept labels
-            shuffle=False)   # our data will be in order 903 benign, 978 malignant
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=33,   #1881 divisible by 33
+        class_mode=None, # predict method doesn't accept labels
+        shuffle=False)   # our data will be in order 903 benign, 978 malignant
     bottleneck_features_train = model.predict_generator(generator, nb_train_samples)
     np.save(open(basedir+'/output/checkpoints/'+modelname+'bottleneck_features_train.npy', 'wb'), bottleneck_features_train)
     print('train bottleneck features saved, shape:', bottleneck_features_train.shape)
     
     generator = datagen.flow_from_directory(
-            validation_data_dir,
-            target_size=(img_width, img_height),
-            batch_size=32,    #481 divisible by 37
-            class_mode=None,  # predict method doesn't accept labels
-            shuffle=False)    # our data will be in order 238 benign, 243 malignant
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,    #481 divisible by 37
+        class_mode=None,  # predict method doesn't accept labels
+        shuffle=False)    # our data will be in order 238 benign, 243 malignant
     bottleneck_features_validation = model.predict_generator(generator, nb_validation_samples)
     np.save(open(basedir+'/output/checkpoints/'+modelname+'bottleneck_features_validation.npy', 'wb'), bottleneck_features_validation)
     print('validation bottleneck features saved, shape:', bottleneck_features_train.shape)
@@ -97,10 +78,15 @@ def train_top_model():
 
     model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
+    #Call backs
+    csvlogger = CSVLogger(basedir+'/output/'+modelname+'_top_weights.csv', separator=',', append=False)
+
     model.fit(train_data, train_labels,
               nb_epoch=nb_epoch, batch_size=batch_size,
               validation_data=(validation_data, validation_labels),
-              callbacks=[])
+              callbacks=[csvlogger],
+              nb_worker=nb_worker, 
+              pickle_safe=True)
     
     model.save_weights(basedir+'/output/checkpoints/'+modelname+'_top_weights.hdf5')
     
